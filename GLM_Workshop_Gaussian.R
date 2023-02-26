7d8e4e1e7d29cc42ca9bbae6a4bfbcad1b9957ab
Bedeffinian <bedeffinian@gmail.com>
2023-02-26 17:28:55 +0100

    starting to create Gaussian script

--- GLM_Workshop_Gaussian.R
+++ GLM_Workshop_Gaussian.R
@@ -6,7 +6,65 @@
 ###################################################
 
 
+## From the introduction we created some fake/simulated data where we know there are differences 
 
+# we will try analysing this data first, then we will download some real-life data to also analyse.
+# the below code will recreate the df 
+
+library(tidyverse)
+
+Year <- seq(from=1950,to=2023,by=1)
+
+Treatment <- c("Control","Treatment 1","Treatment 2")
+
+Rep<- seq(from=1,to=10,by=1)
+
+df1<-expand.grid(Year=Year,Treatment=Treatment,Rep=Rep)
+
+
+Response<-rnorm(n=nrow(df),mean = 15,sd=8)
+
+df1$Response<-Response
+
+# this time we will replace the response column rather than making a new one
+
+df<-df1 %>% 
+  mutate(Response=case_when(Treatment=="Control"~Response*((Year-1949)/30)-10,
+                                    Treatment=="Treatment 1"~Response*((Year-1949)/60)+10,
+                                    Treatment=="Treatment 2"~Response*((Year-1949)/500)-4))
+
+# Modelling wise we may have multiple different expectations from this data, are all treatments changing with time? in the same way?
+# The most complex model would be the interaction of Treatment and Year
+
+lm1<- lm(Response~Treatment*Year,df)
+
+# We can now check the residuals show no patterns
+
+plot(lm1)
+
+lm2<- lm(Response~Treatment+Year,df)
+
+# We can now check the residuals show no patterns
+
+plot(lm2)
+
+lm3<- lm(Response~Year,df)
+
+# We can now check the residuals show no patterns
+
+plot(lm3)
+
+lm4<- lm(Response~Treatment,df)
+
+# We can now check the residuals show no patterns
+
+plot(lm4)
+
+lm5<- lm(Response~1,df)
+
+# We can now check the residuals show no patterns
+
+plot(lm5)
 
 
 
--- GLM_Workshop_Intro.R
+++ GLM_Workshop_Intro.R
@@ -41,21 +41,137 @@ install.packages("dplyr") # you only have to do this when you first want the pac
 library(dplyr) # this needs to be run every new R session when this package is used.
 
 # We can now run functions from the dplyr library, specifically dplyr is a library, which is part of a group of packages called the tidyverse
-# We will use this group of packages for reading data into R (readr), 
+# We will use this group of packages for reading data into R (readr), manipulating data (dplyr) and visualisng data (ggplot2)
+# For GLMs we can use a few different packages, but that is for later on.
+
+# First we can make some data into a dataframe, explore this data, transform and add to it, then we will make some figures with it.
+# R has some very useful random data generation functions 
+
+install.packages("tidyverse") 
+library(tidyverse)
+
+Year <- seq(from=1950,to=2023,by=1)
+Treatment <- c("Control","Treatment 1","Treatment 2")
+Rep<- seq(from=1,to=10,by=1)
+
+# These are three vectors, which we can check information about them with a few simple functions
+
+length(Year)
+summary(Year)
+
+length(Treatment)
+summary(Treatment)
+
+length(Rep)
+summary(Rep)
+
+# We want to combine these vectors so we have a row for each rep, year and treatment, we can do this by expanding the grid
+
+df<-expand.grid(Year=Year,Treatment=Treatment,Rep=Rep)
 
 
+# This df is all the meta data we want for our dataframe that we want to now make up some response data
+
+Response<-rnorm(n=nrow(df),mean = 15,sd=8)
+
+# We can then combine this to our df
+
+df$Response<-Response
+
+# We can check the data we have using a base plot function
+
+plot(df)
+
+# Here we can see that the response is totally random, regardless of treatment and year
+# for interesting plotting we can change this 
+# For manipulation we can use something called pipes %>% which push the output from one line into the function on the next line
+# we can change the values of response by the values of year and treatment using the case_when function to create clauses similar to if else statements
+# The mutate function can be used to create new columns based on current columns or other values/objects
 
 
+df_1<-df %>% 
+  mutate(Response_updated=case_when(Treatment=="Control"~Response*((Year-1949)/30)-10,
+                                    Treatment=="Treatment 1"~Response*((Year-1949)/60)+10,
+                                    Treatment=="Treatment 2"~Response*((Year-1949)/500)-4))
 
 
+# We can now calculate some summary statistics such as mean or standard deviation using group_by and summarise
 
 
+df_1_summary<-df_1 %>% 
+  group_by(Treatment) %>% 
+  summarise(MeanResponse=mean(Response_updated),
+            SDResponse=sd(Response_updated))
+
+df_1_summary
+
+df_1_summary_year<-df_1 %>% 
+  group_by(Treatment,Year) %>% 
+  summarise(MeanResponse=mean(Response_updated),
+            SDResponse=sd(Response_updated))
+
+df_1_summary_year
 
 
+# Now lets try and plot these data, for more complex plots ggplot2 is by far the best package
+# we first make a blank ggplot saying what data we will include
+# then we want to add layers to the plot for what we want to plot and how we want it to look
+# for elements of the plot that change with data from the df we use the aes() function
 
 
+ggplot(df_1)+
+  geom_point(aes(x=Year,y=Response_updated,colour=Treatment))
 
 
+# This looks good but could be Nicer there are many geom types to dictate the type of plot
+
+ggplot(df_1)+
+  geom_density(aes(x=Response_updated,colour=Treatment))
+
+ggplot(df_1)+
+  geom_col(aes(x=Year,y=Response_updated,fill=Treatment),alpha=0.5)
 
 
+# we can also add new data into the same plot so we could use the summary data too, for this we add the data to the geom we want it used for
+
+ggplot()+
+  geom_point(data=df_1,mapping=aes(x=Year,y=Response_updated,colour=Treatment),
+             alpha=0.4)+
+  geom_ribbon(data=df_1_summary_year,mapping=aes(x=Year,ymax=MeanResponse+SDResponse,
+                                                 ymin=MeanResponse-SDResponse,fill=Treatment),
+              alpha=0.4)+
+  geom_line(data=df_1_summary_year,mapping=aes(x=Year,y=MeanResponse,colour=Treatment))
+
+# we can now change the style and appearance 
+# theme is used to change appearances
+
+ggplot()+
+  geom_point(data=df_1,mapping=aes(x=Year,y=Response_updated,colour=Treatment),
+             alpha=0.4)+
+  geom_ribbon(data=df_1_summary_year,mapping=aes(x=Year,ymax=MeanResponse+SDResponse,
+                                                 ymin=MeanResponse-SDResponse,fill=Treatment),
+              alpha=0.4)+
+  geom_line(data=df_1_summary_year,mapping=aes(x=Year,y=MeanResponse,colour=Treatment))+
+  scale_color_manual(values=c("darkcyan","darkorange","grey30"))+
+  scale_fill_manual(values=c("darkcyan","darkorange","grey30"))+
+  labs(x="Year",y="Response Variable")+
+  theme_classic()
+
+# We could even separate plots by another column 
+
+ggplot()+
+  geom_point(data=df_1,mapping=aes(x=Year,y=Response_updated,colour=Treatment),
+             alpha=0.4)+
+  geom_ribbon(data=df_1_summary_year,mapping=aes(x=Year,ymax=MeanResponse+SDResponse,
+                                                 ymin=MeanResponse-SDResponse,fill=Treatment),
+              alpha=0.4)+
+  geom_line(data=df_1_summary_year,mapping=aes(x=Year,y=MeanResponse,colour=Treatment))+
+  facet_wrap(~Rep, nrow=2)+
+  scale_color_manual(values=c("darkcyan","darkorange","grey30"))+
+  scale_fill_manual(values=c("darkcyan","darkorange","grey30"))+
+  labs(x="Year",y="Response Variable")+
+  theme_classic()
+
+# google is full of different elements that can be changed within a plot
+# you can google exactly what you want, then normally stackoverflow answers or blogs will help most
 
